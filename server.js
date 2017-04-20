adefc077d5480831170a5f6acf56929d5b48f13f
David Freeman <davidfreeman@ksu-wireless-6-6.campus.ksu.edu>
2017-04-20 14:21:25 -0500

    added tests

--- server.js
+++ server.js
@@ -7,16 +7,28 @@ const PORT = 3000;
 
 // Requires
 var fs = require('fs');
-var http = require('http');
+var https = require('https');
 var express = require('express');
+var config = require('./config.json');
 
 // The Express app
 var app = express();
 
+//The webserver
+options = {
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+}
+var server = https.createServer(options, app)
+
 // Serve files from public folder
 app.use(express.static('public'));
 
+//Set up Authentication
+var authCAS = require('auth-cas');
+var auth = new AuthCAS(config.host, config.casHost);
+
 // Start the server
-app.listen(PORT, function(){
+server.listen(PORT, function(){
   console.log(PORT);
 });
